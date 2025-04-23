0089c0cb4c9f801996a4ce0f57d2ac210efb3dfa
Sushil-Ohol-Personel <123925553+Sushil-Ohol-Personel@users.noreply.github.com>
2025-04-23 16:17:07 +0530

    Test with changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "sushil-bucket-s3" {
-  bucket = "sushil-backend-bucket-terraform-aws-vijay"
+  bucket = "sushil-backend-bucket-terraform-aws-vijay-2"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
